420bfd5292b121f99803bdffc915cd1df8879e91
Madd Sauer <github@sauer.ms>
2023-02-26 22:23:54 +0100

    Update K3s-init.sh

--- K3s-init.sh
+++ K3s-init.sh
@@ -10,3 +10,5 @@ curl -sfL https://get.k3s.io | K3S_URL=https://swfs-01:6443 K3S_TOKEN=SECRET sh 
 
 # all nodes
 setfacl -m u:fedora:rw /etc/rancher/k3s/k3s.yaml
+
+# README => https://docs.k3s.io/cluster-access
