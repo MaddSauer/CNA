787b24a1ee6187c52a4aaac4e81130a0419d4b2f
Madd Sauer <github@sauer.ms>
2023-03-04 08:03:34 +0100

    Update 1_Fedora-init.sh

--- 1_Fedora-init.sh
+++ 1_Fedora-init.sh
@@ -3,7 +3,7 @@
 # 2023-02-25  madd  init
 
 dnf -y update
-dnf -y install vim tmux wget atop htop etckeeper acl helm
+dnf -y install vim tmux wget atop htop etckeeper acl helm s3fs-fuse
 etckeeper init
 etckeeper commit init
 
